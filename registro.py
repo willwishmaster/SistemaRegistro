772bb671b0408ea8cfb9bf2864669b9d2987d98a
Wilson Velasquez <wilcvelasquez@hotmail.com>
2020-11-16 09:11:31 -0500

    Segundo commit

---
+++ registro.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+def main():
+    dialogo = Tk();
+    dialogo.geometry("300x250")
+    
+    dialogo.title("Sistema de Registro")
+    Label(text="Sistema de Registro",bg="grey", width="300", height="2", font=("Calibri",13)).pack()
+    Label(text="").pack()
+    Button(text="Iniciar sesion", height="2", width="30").pack()
+    
+    dialogo.mainloop()
+    
+main()
