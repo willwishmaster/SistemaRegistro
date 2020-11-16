697e15a80ca8cf484e386c9c2d4daefc234ae122
Wilson Velasquez <wilcvelasquez@hotmail.com>
2020-11-16 09:29:31 -0500

    Tercer commit

--- registro.py
+++ registro.py
@@ -8,6 +8,8 @@ def main():
     Label(text="Sistema de Registro",bg="grey", width="300", height="2", font=("Calibri",13)).pack()
     Label(text="").pack()
     Button(text="Iniciar sesion", height="2", width="30").pack()
+    Label(text="").pack()
+    Button(text="Registrar", height="2", width="30").pack()
     
     dialogo.mainloop()
     
